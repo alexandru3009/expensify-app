805f7ac528b60c259efa53e3260d6c3d9bb918a7
Alex <alexandru.dascalescu90@gmail.com>
2018-10-26 11:27:32 +0300

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -14,18 +14,7 @@ const store = configureStore();
 store.dispatch(addExpense( { description: 'Water bill', amount:500 }));
 store.dispatch(addExpense( { description: 'Gas bill',createdAt:1000 }));
 store.dispatch(addExpense( { description: 'Rent',amount: 100300}));
-// store.dispatch(setTextFilter('water'));
 
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000)
-
-// store.subscribe(() => {
-//     const state = store.getState();
-//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//     console.log(visibleExpenses);
-// });
-//----------subscribe echivalenta cu cea de jos
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
